82a75d1adc68d3b11c3d141f4ea89db737938043
Pál Patrik Varga <VargaPalPatrik@student.tudelft.nl>
2022-03-05 11:58:15 +0100

    Docs: javadocs for the Answer class
    Also, setter methods

--- commons/src/main/java/commons/models/Answer.java
+++ commons/src/main/java/commons/models/Answer.java
@@ -14,25 +14,45 @@ public class Answer {
     private CompareType compareType;
     private AnswerType answerType;
 
+    /**
+     * A constructor for a long type answer. The type of this instance is *not* tied to just long.
+     * @param longAnswer the answer itself
+     */
     public Answer(Long longAnswer){
         this.longAnswer = longAnswer;
         this.answerType = LONG;
     }
 
+    /**
+     * A constructor for an activity type answer. The type of this instance is *not* tied to just activity.
+     * @param activity the answer itself
+     */
     public Answer(Activity activity){
         this.activity = activity;
         this.answerType = ACTIVITY;
     }
 
+    /**
+     * A constructor for a compare type answer. The type of this instance is *not* tied to just compare type.
+     * @param compareType the answer itself
+     */
     public Answer(CompareType compareType){
         this.compareType = compareType;
         this.answerType = COMPARETYPE;
     }
 
+    /**
+     * A getter for the type of the answer.
+     * @return the type of the answer
+     */
     public AnswerType getAnswerType() {
         return answerType;
     }
 
+    /**
+     * A getter for the answer
+     * @return the answer
+     */
     public Object getAnswer(){
         switch (answerType) {
             case LONG:
@@ -41,5 +61,32 @@ public class Answer {
                 return activity;
         }
         return compareType;
+    }
+
+    /**
+     * A setter for the answer (long)
+     * @param answer the new answer
+     */
+    public void setAnswer(Long answer){
+        this.longAnswer = answer;
+        this.answerType = LONG;
+    }
+
+    /**
+     * A setter for the answer (activity)
+     * @param answer the new answer
+     */
+    public void setAnswer(Activity answer){
+        this.activity = answer;
+        this.answerType = ACTIVITY;
+    }
+
+    /**
+     * A setter for the answer (compare type)
+     * @param answer the new answer
+     */
+    public void setAnswer(CompareType answer){
+        this.compareType = answer;
+        this.answerType = COMPARETYPE;
     }
 }
