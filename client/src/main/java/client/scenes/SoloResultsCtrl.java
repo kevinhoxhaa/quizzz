ee62af6619f07ed6bfb147bc6dc87bfc24563514
Calin Georgescu <georgescu-2@student.tudelft.nl>
2022-03-12 16:14:41 +0100

    Feat : added solo results page controller

---
+++ client/src/main/java/client/scenes/SoloResultsCtrl.java
@@ -0,0 +1,53 @@
+package client.scenes;
+
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class SoloResultsCtrl {
+
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    /**
+     * Creates a controller for the solo results page screen, with the given server and mainCtrl parameters.
+     * @param server
+     * @param mainCtrl
+     */
+    @Inject
+    public SoloResultsCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.server = server;
+        this.mainCtrl = mainCtrl;
+    }
+
+    @FXML
+    private Text score;
+
+    @FXML
+    private Text personalBest;
+
+    @FXML
+    private Button quit;
+
+    /**
+     * Setups the page quit button that redirects to the main page, and fills in the score and personal best
+     *
+     */
+
+    public void setup() {
+        quit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                mainCtrl.showHome();
+            }
+        });
+
+        score.setText( String.format( "%d", mainCtrl.getSoloScore()) );
+        personalBest.setText( String.format( "%d", mainCtrl.getPersonalBest()) );
+    }
+
+}
