7735e430b7df171165f1432f1724ea4c75ef324f
Bink Boëtius <B.J.Boetius@student.tudelft.nl>
2022-03-29 15:32:29 +0200

    Fix: Add back deleted method

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -204,6 +204,31 @@ public class ServerUtils {
                 .target(serverUrl).path("api/users/"+user.id)
                 .request(APPLICATION_JSON)
                 .accept(APPLICATION_JSON)
+                .delete(MultiplayerUser.class);
+    }
+
+    /**
+     * A method that posts the answer of the user in the repository
+     * @param serverUrl
+     * @param gameIndex
+     * @param userId
+     * @param questionIndex
+     * @param question
+     * @return list of the users who got the question right
+     */
+    public List<MultiplayerUser> answerQuestion(String serverUrl, int gameIndex,
+                                                long userId, int questionIndex, Question question) {
+        String path = String.format(
+                "api/games/%d/user/%d/question/%d",
+                gameIndex,
+                userId,
+                questionIndex
+        );
+
+        return ClientBuilder.newClient(new ClientConfig())
+                .target(serverUrl).path(path)
+                .request(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
                 .post(Entity.entity(question, APPLICATION_JSON), new GenericType<List<MultiplayerUser>>() {});
     }
 
