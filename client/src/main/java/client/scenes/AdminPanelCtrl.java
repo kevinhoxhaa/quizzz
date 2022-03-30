e5f3498be02033147610706cdbd5e1d813abc8a7
Calin Georgescu <georgescu-2@student.tudelft.nl>
2022-03-30 15:45:53 +0200

    Beggining of admin panel

--- client/src/main/java/client/scenes/AdminPanelCtrl.java
+++ client/src/main/java/client/scenes/AdminPanelCtrl.java
@@ -1,4 +1,25 @@
 package client.scenes;
 
+import client.utils.ServerUtils;
+import commons.entities.Activity;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class AdminPanelCtrl {
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    @FXML
+    private Button addActivity;
+
+    public AdminPanelCtrl ( ServerUtils server, MainCtrl mainCtrl ) {
+        this.server = server;
+        this.mainCtrl = mainCtrl;
+    }
+
+    @FXML
+    public void onAddActivityButton ( String title, int consumption, String source ) {
+        server.addActivity( mainCtrl.getServerUrl(), mainCtrl.getGameIndex(), new Activity( title, consumption, source ) );
+    }
+
 }
